f4d5dbfd5ca1119d7398726c9763f3f84815938a
Philipp Reh <sefi@s-e-f-i.de>
2010-11-30 00:36:02 +0100

    Added -Wshadow.

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,14 +1,33 @@
-#include <awl/backends/x11/window_system.hpp>
-#include <awl/backends/x11/window_instance.hpp>
+#include <awl/window/create_system.hpp>
+#include <awl/window/system.hpp>
+#include <awl/window/instance.hpp>
 #include <awl/window/parameters.hpp>
+#include <fcppt/time/sleep_any.hpp>
+#include <fcppt/chrono/seconds.hpp>
 
 int main()
 {
-	awl::backends::x11::window_system ws;
+	awl::window::system_ptr const window_system(
+		awl::window::create_system()
+	);
 
-	awl::window::instance_ptr wind = 
-		ws.create(
-			awl::window::parameters());
+	awl::window::instance_ptr const wind(
+		window_system->create(
+			awl::window::parameters()
+			.size(
+				awl::window::dim(
+					1024,
+					768
+				)
+			)
+		)
+	);
 	
 	wind->show();
+
+	fcppt::time::sleep_any(
+		fcppt::chrono::seconds(
+			1
+		)
+	);
 }
