01bc3ecedb9fe61fb1eacbe28ef39115a9ef8eb7
Philipp Reh <sefi@s-e-f-i.de>
2011-01-21 15:04:34 +0100

    Added windows::system_metrics.

---
+++ include/awl/backends/windows/system_metrics.hpp
@@ -0,0 +1,23 @@
+#ifndef AWL_BACKENDS_WINDOWS_SYSTEM_METRICS_HPP_INCLUDED
+#define AWL_BACKENDS_WINDOWS_SYSTEM_METRICS_HPP_INCLUDED
+
+#include <awl/symbol.hpp>
+
+namespace awl
+{
+namespace backends
+{
+namespace windows
+{
+
+AWL_SYMBOL
+int
+system_metrics(
+	int
+);
+
+}
+}
+}
+
+#endif
---
+++ src/backends/windows/system_metrics.cpp
@@ -0,0 +1,25 @@
+#include <awl/backends/windows/system_metrics.hpp>
+#include <awl/backends/windows/windows.hpp>
+#include <awl/exception.hpp>
+#include <fcppt/text.hpp>
+
+int
+awl::backends::windows::system_metrics(
+	int const _what
+)
+{
+	int const ret(
+		::GetSystemMetrics(
+			_what
+		)
+	);
+
+	if(
+		ret == 0
+	)
+		throw awl::exception(
+			FCPPT_TEXT("GetSystemMetrics() failed!")
+		);
+	
+	return ret;
+}
