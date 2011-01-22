ef5a630d0458b76794a74311740518c6f4f400b1
Philipp Reh <sefi@s-e-f-i.de>
2011-01-22 16:04:02 +0100

    Removed system::timer.

---
+++ src/backends/windows/system/event/handle.cpp
@@ -0,0 +1,9 @@
+#include <awl/backends/windows/system/event/handle.hpp>
+
+awl::backends::windows::system::event::handle::handle()
+{
+}
+
+awl::backends::windows::system::event::handle::~handle()
+{
+}
---
+++ src/backends/windows/system/event/original_handle.cpp
@@ -0,0 +1,64 @@
+#include <awl/backends/windows/system/event/original_handle.hpp>
+#include <awl/backends/windows/windows.hpp>
+#include <awl/exception.hpp>
+#include <fcppt/text.hpp>
+
+awl::backends::windows::system::event::original_handle::original_handle(
+	system::event::handle_destroy_callback const &_on_destroy
+)
+:
+	on_destroy_(_on_destroy),
+	handle_(
+		::CreateEvent(
+			NULL,
+			FALSE,
+			FALSE,
+			NULL
+		)
+	)
+{
+	if(
+		handle_ == NULL
+	)
+		throw awl::exception(
+			FCPPT_TEXT("CreateEvent() failed!")
+		);
+}
+
+awl::backends::windows::system::event::original_handle::~original_handle()
+{
+	::CloseHandle(
+		handle_
+	);
+
+	on_destroy_(
+		handle_
+	);
+}
+
+HANDLE
+awl::backends::windows::system::event::original_handle::get() const
+{
+	return handle_;
+}
+
+bool
+awl::backends::windows::system::event::original_handle::ready() const
+{
+	DWORD const ret(
+		::WaitForSingleObject(
+			handle_,
+			0u
+		)
+	);
+
+	if(
+		ret == WAIT_FAILED
+	)
+		throw awl::exception(
+			FCPPT_TEXT("WaitForSingleObject() failed!")
+		);
+
+	return
+		ret == WAIT_OBJECT_0;
+}
