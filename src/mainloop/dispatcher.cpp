ba1ba8ec394b374e53af4ab2dbd22af93948da68
Philipp Reh <sefi@s-e-f-i.de>
2012-05-25 12:40:53 +0200

    Fixed filenames and removed obsolete stuff.

--- src/mainloop/dispatcher.cpp
+++
@@ -1,9 +0,0 @@
-#include <awl/mainloop/dispatcher.hpp>
-
-awl::mainloop::dispatcher::dispatcher()
-{
-}
-
-awl::mainloop::dispatcher::~dispatcher()
-{
-}
--- src/mainloop/io_service.cpp
+++
@@ -1,9 +0,0 @@
-#include <awl/mainloop/io_service.hpp>
-
-awl::mainloop::io_service::io_service()
-{
-}
-
-awl::mainloop::io_service::~io_service()
-{
-}
