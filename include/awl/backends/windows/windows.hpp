e2ff48a7eb49637a7dfa2fd0925b4153db6fb693
Philipp Reh <sefi@s-e-f-i.de>
2012-07-21 15:53:29 +0200

    Fixed windows.hpp

--- include/awl/backends/windows/windows.hpp
+++ include/awl/backends/windows/windows.hpp
@@ -1,6 +1,12 @@
 #ifndef AWL_BACKENDS_WINDOWS_WINDOWS_HPP_INCLUDED
 #define AWL_BACKENDS_WINDOWS_WINDOWS_HPP_INCLUDED
 
-#include <fcppt/config/include_windows.hpp>
+#if !defined(NOMINMAX)
+#define NOMINMAX
+#endif
+
+#include <fcppt/config/external_begin.hpp>
+#include <Windows.h>
+#include <fcppt/config/external_end.hpp>
 
 #endif
