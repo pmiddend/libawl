0ed24fac065df383f425a41e5dd8497eee646df6
Philipp Reh <sefi@s-e-f-i.de>
2013-12-27 20:50:54 +0100

    Fixed optional include

--- include/awl/backends/x11/system/event/optional_processor_ref.hpp
+++ include/awl/backends/x11/system/event/optional_processor_ref.hpp
@@ -1,29 +1,8 @@
 #ifndef AWL_BACKENDS_X11_SYSTEM_EVENT_OPTIONAL_PROCESSOR_REF_HPP_INCLUDED
 #define AWL_BACKENDS_X11_SYSTEM_EVENT_OPTIONAL_PROCESSOR_REF_HPP_INCLUDED
 
-#include <awl/backends/x11/system/event/processor_fwd.hpp>
-#include <fcppt/optional_fwd.hpp>
+#include <awl/backends/x11/system/event/optional_processor_ref_fwd.hpp>
+#include <fcppt/optional_impl.hpp>
 
-
-namespace awl
-{
-namespace backends
-{
-namespace x11
-{
-namespace system
-{
-namespace event
-{
-
-typedef fcppt::optional<
-	awl::backends::x11::system::event::processor &
-> optional_processor_ref;
-
-}
-}
-}
-}
-}
 
 #endif
